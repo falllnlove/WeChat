4df40689b4348d113364c824d0bc4a62854865af
falllnlove <858742211@qq.com>
2019-11-28 14:07:17 +0800

    增加"评论列表下拉刷新","全文展开,收起"
    增加"评论列表下拉刷新","全文展开,收起"

--- pages/room_par_discuss/room_par_discuss.js
+++ pages/room_par_discuss/room_par_discuss.js
@@ -10,6 +10,8 @@ Page({
     room_id: 8685168,
     pageindex : 0,
     discuss_list:null,
+    full : null,
+    text :null
   },
 
   /**
@@ -64,7 +66,29 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+    wx.showToast({
+      title: '加载中...',
+      mask: true,
+      icon: 'loading'
+    })
+    var that = this
+    that.setData({
+      pageindex : that.data.pageindex+1
+    })
+    wx.request({
+      url: 'https://m.ctrip.com/restapi/soa2/12455/json/BnbCommentList?_fxpcqlniredt=09031174410421181703&__gw_appid=99999999&__gw_ver=1.0&__gw_from=10320662860&__gw_platform=H5',
+      data: `{ "pid": ${that.data.room_id}, "searchType": 2, "pageindex": ${that.data.pageindex}, "head": { "cid": "09031174410421181703", "ctok": "", "cver": "1.0", "lang": "01", "sid": "8888", "syscode": "09", "auth": null, "extension": [{ "name": "webp", "value": "1" }, { "name": "cityid", "value": "17" }, { "name": "platform", "value": "Other" }, { "name": "source", "value": "2" }, { "name": "protocal", "value": "https" }] }, "contentType": "json" }`,
+      method: 'post',
+      success: res => {
+        res.data.clist.forEach((item,index) => {
+          that.data.discuss_list.clist.push(item)
+        })
+        that.setData({
+          discuss_list : that.data.discuss_list
+        })
+      }
+    })
+
   },
 
   /**
@@ -79,18 +103,40 @@ Page({
    */
   discuss(){
     var that = this
-    console.log(that.data.room_id)
-    console.log(that.data.pageindex)
     wx.request({
       url: 'https://m.ctrip.com/restapi/soa2/12455/json/BnbCommentList?_fxpcqlniredt=09031174410421181703&__gw_appid=99999999&__gw_ver=1.0&__gw_from=10320662860&__gw_platform=H5',
       data: `{ "pid": ${that.data.room_id}, "searchType": 2, "pageindex": ${that.data.pageindex}, "head": { "cid": "09031174410421181703", "ctok": "", "cver": "1.0", "lang": "01", "sid": "8888", "syscode": "09", "auth": null, "extension": [{ "name": "webp", "value": "1" }, { "name": "cityid", "value": "17" }, { "name": "platform", "value": "Other" }, { "name": "source", "value": "2" }, { "name": "protocal", "value": "https" }] }, "contentType": "json" }`,
       method:'post',
       success: res => {
-        console.log(res.data)
         that.setData({
           discuss_list : res.data
         })
       }
     })
   },
+
+  /**
+   * 监听全文点击事件
+   */
+  full_text(e){
+    var that = this
+    if (e.currentTarget.dataset.index == that.data.full){
+      that.setData({
+        full : null
+      })
+    }else {
+      that.setData({
+        full: e.currentTarget.dataset.index
+      })
+    }
+    if (e.currentTarget.dataset.text == that.data.text) {
+      that.setData({
+        text: 1
+      })
+    } else {
+      that.setData({
+        text: e.currentTarget.dataset.text
+      })
+    }
+  }
 })
